95b9fce19712f9b747eea194d8d50e6da4df11d6
rajib18197 <rajib18197@gmail.com>
2024-03-18 21:50:08 +0600

    design home page

--- app/components/BlogsList.jsx
+++ app/components/BlogsList.jsx
@@ -1,5 +1,6 @@
 import getBlogsData from "@/lib/blogs";
 import BlogRow from "./BlogRow";
+import Link from "next/link";
 
 // https://media.licdn.com/dms/image/D4E22AQHx50SHtAqgQA/feedshare-shrink_1280/0/1710265722936?e=1713398400&v=beta&t=1jMqOAR-FQhkbixGPN7wtGxcyNO9y8zj9RAAB8gNQUc
 
@@ -7,14 +8,20 @@ export default function BlogsList() {
   const blogs = getBlogsData();
   return (
     <div className="flex flex-col gap-20 mt-4">
-      <div className="Section-title">
-        <h3 className="title">
-          {/* <span className="brand brand-1">R</span>
-          <span className="brand brand-2">A</span> */}
-          <span className="name">The Then Mind</span>
-          {/* <span className="brand brand-3">J</span>
-          <span className="brand brand-4">U</span> */}
-        </h3>
+      <div className="flex justify-between">
+        <Link href={"/"} className="grid grid-cols-2 gap-8 items-center">
+          <span className="w-[60px] h-[60px] self-center rounded-full bg-pink-600"></span>
+          <div className="flex flex-col justify-center">
+            <h3 className="uppercase text-2xl">Ra Ju</h3>
+            <p className="text-gray-600 ">@obsession</p>
+          </div>
+        </Link>
+
+        <div className="Section-title">
+          <h3 className="title">
+            <span className="name">The Then Mind</span>
+          </h3>
+        </div>
       </div>
       <div className="flex flex-col gap-28">
         {blogs.map((blog) => (
