9c7eec9a3ffb0af643e50b16dfe398d265b239cf
rajib18197 <rajib18197@gmail.com>
2024-03-23 22:04:38 +0600

    experiment some stuff

--- app/blogs/[blogId]/page.jsx
+++ app/blogs/[blogId]/page.jsx
@@ -28,6 +28,7 @@ export default async function Blog({ params }) {
   );
 }
 
+// <!-- Every Problem is An Opportunity to Show Which Super Hero You Are  -->
 // In SSR, components run on the server and on the client also but in this case this is unnecessary as there is no interactiveness associated with those components. React SSR fixed this issue by omitting these components code from sending and running in the browser and reducing the bundle size.
 
 // API Calls / file reading / db calls → generate HTML → download JS (only client components, excluded generated HTML (Thanks to server components)) → hydration
